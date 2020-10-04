647c270c1ba698d6a9403b7158aefd272ec1bb2a
Optional11 <60046905+Optional11@users.noreply.github.com>
2020-10-04 21:08:49 +0200

    First Commit

---
+++ resources/js/apollo.config.js
@@ -0,0 +1,19 @@
+import Vue from 'vue';
+import ApolloClient from 'apollo-boost';
+import VueApollo from 'vue-apollo';
+
+Vue.use(VueApollo);
+
+const apolloClient = new ApolloClient({
+    uri: 'http://127.0.0.1:8000/graphql',
+    headers: {
+        'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]')
+            .content,
+    },
+    credentials: 'include',
+    onError: (err) => console.log('A Global Error Handler'),
+});
+
+export default new VueApollo({
+    defaultClient: apolloClient,
+});
